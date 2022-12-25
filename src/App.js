13229fabe9534736856bb690e9ab013fc0389c69
Dhananjaya <idhananjay@wiley.com>
2022-12-25 21:15:02 +0530

    queries

--- src/App.js
+++ src/App.js
@@ -6,25 +6,35 @@ import { Contact } from "./pages/Contact";
 import { Navbar } from "./Navbar";
 import { Profile } from "./pages/Profile";
 import { useState, createContext } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const AppContext = createContext();
 
 function App() {
   const [username, setUserName] = useState("Draco");
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
   return (
     <div className="App">
       <AppContext.Provider value={{ username, setUserName }}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
-          </Routes>
-        </Router>
+        <QueryClientProvider client={client}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
+            </Routes>
+          </Router>
+        </QueryClientProvider>
       </AppContext.Provider>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
+
 export const Home = () => {
-  return <h1> THIS IS THE HOME PAGE</h1>;
+  const { data, isLoading, refetch } = useQuery(["cat"], () => {
+    return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h1>
+        Here is a random cat fact!
+        <div>
+          <button onClick={() => refetch()}>Refetch</button>
+        </div>
+        <p>{data?.fact}</p>
+      </h1>
+    </div>
+  );
 };
