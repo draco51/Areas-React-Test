238e3c6e6f14c7bb0ce8c54f508aaa22122bc179
Dhananjaya <idhananjay@wiley.com>
2022-12-25 19:53:35 +0530

    init 2

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import { Home } from "./pages/Home";
 import { Menu } from "./pages/Menu";
 import { Contact } from "./pages/Contact";
 import { Navbar } from "./Navbar";
+import { Profile } from "./pages/Profile";
+import { useState, createContext } from "react";
+
+export const AppContext = createContext();
 
 function App() {
+  const [username, setUserName] = useState("Draco");
+
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
-        </Routes>
-      </Router>
+      <AppContext.Provider value={{ username, setUserName }}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
+          </Routes>
+        </Router>
+      </AppContext.Provider>
     </div>
   );
 }
---
+++ src/components/ChangeProfile.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useContext } from "react";
+import { AppContext } from "../App";
+
+export const ChangeProfile = () => {
+  const { setUserName } = useContext(AppContext);
+  const [newUserName, setNewUserName] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(event) => {
+          setNewUserName(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setUserName(newUserName);
+        }}
+      >
+        Change Username
+      </button>
+    </div>
+  );
+};
